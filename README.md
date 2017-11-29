dc1d82d7db3aff42341fc8c90d382b251bea1902
sumioku_r <rikuson298@gmail.com>
2017-11-29 11:07:50 +0900

    Chrome上で実行可能に

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Tutorial: Intro To React の作業用ワークスペースです。
 
 ・進捗
 
-- [ ] Overview
-- [ ] Lifting State Up
-- [ ] Storing A History
+- [x] CodePen上で実行可能
+- [x] Chrome上で実行可能(webpackを用いてjsxをコンパイル)
+- [ ] チャレンジ課題完了
 
---
+++ bundle.js
@@ -0,0 +1,216 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+function Square(props) {
+    return React.createElement(
+        "button",
+        { className: "square", onClick: () => props.onClick() },
+        props.value
+    );
+}
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return React.createElement(Square, { value: this.props.squares[i], onClick: () => this.props.onClick(i) });
+    }
+
+    render() {
+        return React.createElement(
+            "div",
+            null,
+            React.createElement(
+                "div",
+                { className: "board-row" },
+                this.renderSquare(0),
+                this.renderSquare(1),
+                this.renderSquare(2)
+            ),
+            React.createElement(
+                "div",
+                { className: "board-row" },
+                this.renderSquare(3),
+                this.renderSquare(4),
+                this.renderSquare(5)
+            ),
+            React.createElement(
+                "div",
+                { className: "board-row" },
+                this.renderSquare(6),
+                this.renderSquare(7),
+                this.renderSquare(8)
+            )
+        );
+    }
+}
+
+class Game extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null)
+            }],
+            xIsNext: true,
+            stepNumber: 0
+        };
+    }
+
+    handleClick(i) {
+        var history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[this.state.stepNumber];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                squares: squares
+            }]),
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: step % 2 === 0
+        });
+    }
+
+    render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ? 'Go to Move #' + move : 'Go to Game start';
+            return React.createElement(
+                "li",
+                { key: move },
+                React.createElement(
+                    "button",
+                    { href: "#", onClick: () => this.jumpTo(move) },
+                    desc
+                )
+            );
+        });
+
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
+        return React.createElement(
+            "div",
+            { className: "game" },
+            React.createElement(
+                "div",
+                { className: "game-board" },
+                React.createElement(Board, {
+                    squares: current.squares,
+                    onClick: i => this.handleClick(i)
+                })
+            ),
+            React.createElement(
+                "div",
+                { className: "game-info" },
+                React.createElement(
+                    "div",
+                    null,
+                    status
+                ),
+                React.createElement(
+                    "ol",
+                    null,
+                    moves
+                )
+            )
+        );
+    }
+}
+
+ReactDOM.render(React.createElement(Game, null), document.getElementById('root'));
+
+function calculateWinner(squares) {
+    const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+/***/ })
+/******/ ]);
